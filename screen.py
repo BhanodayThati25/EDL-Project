10306b8121e40d1173f0628f967663d8df80f22d
Thati Bhanoday <200020051@iitdh.ac.in>
2023-03-20 11:05:33 +0530

    changed keypad

--- screen.py
+++ screen.py
@@ -88,7 +88,7 @@ class Application(tk.Frame):
             ".", "0", "⌫"
         ]
         for i in range(len(buttons)):
-            button = tk.Button(self.keypad_window, text=buttons[i],font=("Arial", 14), width=10, height=3,relief='ridge')
+            button = tk.Button(self.keypad_window, text=buttons[i],font=("Arial", 14), width=9, height=3,relief='ridge')
             button.grid(row=((i // 3 + 2)-1), column=i % 3,padx=6,pady=3)
             button.bind("<Button-1>", self.keypad_button_click)
 
